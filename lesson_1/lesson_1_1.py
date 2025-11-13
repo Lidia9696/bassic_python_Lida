a91e39d9af99ee1d7a61de37c245f828a34d8b60
Lidia Lankova <you@example.com>
2025-11-13 11:26:36 +0300

    Lesson 1

--- lesson_1/lesson_1_1.py
+++ lesson_1/lesson_1_1.py
@@ -1,1 +1,26 @@
-print("Привет, мир")
+# a = 7
+# str_1 = "легко в ИТ"
+# b = a
+# a = 8
+# print(b)
+#
+# a = b = c = 0
+# a, b, c = 0, 0, 0
+#
+# a, b = 8, 10
+# a, b = b, a
+# print(a)
+# print(b)
+#
+# print(type(a))
+# print(type(b))
+# print(type(str_1))
+
+a = c = 4
+b, d = "Какая-нибудь строка", "Вторая строка"
+
+print(id(a))
+print(id(b))
+print(id(c))
+print(id(d))
+
